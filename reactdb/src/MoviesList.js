ca7e2440544f403595bdbafd0fef9dc6fe01bd36
Evgeny Kasian <ekasian@outlook.com>
2018-07-19 14:46:45 -0700

    finished creating a barebones movie db react app

---
+++ reactdb/src/MoviesList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=30dddc8856970751018cd4a2f17ad49f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movies } = this.state;
+    return <MovieGrid>{movies.map(movie => <Movie key={movie.id} movie={movie} />)}</MovieGrid>;
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
